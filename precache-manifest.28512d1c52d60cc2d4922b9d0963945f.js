61dda6b2401e67f06d919e19e602191428e70c1d
Jon B <jonlondontech@gmail.com>
2020-03-11 01:27:53 +0000

    Updates

---
+++ precache-manifest.28512d1c52d60cc2d4922b9d0963945f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942bd17ef23f9b79665a2981bcb4aa82",
+    "url": "/automotive_gsap_react/index.html"
+  },
+  {
+    "revision": "fb57f261bc473406b0e8",
+    "url": "/automotive_gsap_react/static/css/main.412f7ad3.chunk.css"
+  },
+  {
+    "revision": "c7d86c320dadbed6be11",
+    "url": "/automotive_gsap_react/static/js/2.1c102e4b.chunk.js"
+  },
+  {
+    "revision": "745169d1d7e72568a5e36dc0bf145796",
+    "url": "/automotive_gsap_react/static/js/2.1c102e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb57f261bc473406b0e8",
+    "url": "/automotive_gsap_react/static/js/main.e958dfa6.chunk.js"
+  },
+  {
+    "revision": "faf9090ba4e0205b80b3",
+    "url": "/automotive_gsap_react/static/js/runtime-main.a3579c33.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/automotive_gsap_react/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/automotive_gsap_react/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "3af21423660ad8cbfc0538de122d6a72",
+    "url": "/automotive_gsap_react/static/media/aston.3af21423.png"
+  },
+  {
+    "revision": "870c9fe2165b7e1319a869a3ffb88f21",
+    "url": "/automotive_gsap_react/static/media/ferrari.870c9fe2.png"
+  },
+  {
+    "revision": "1cc69d4c48bd988b200a7d98833de73a",
+    "url": "/automotive_gsap_react/static/media/mercedes.1cc69d4c.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/automotive_gsap_react/static/media/up-arrow-circle.d6c4e78d.svg"
+  }
+]);
